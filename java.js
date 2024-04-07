effce5ce4d8af183148413ad0615756ca1d1a75a
BradQuenneville <quennevillebradley@gmail.com>
2024-04-06 23:20:37 -0700

    Complete program.
    Undefined shows up in log.

--- java.js
+++ java.js
@@ -4,90 +4,56 @@ const choices = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice() {
     return computerRPS = choices[Math.floor(Math.random() * choices.length)];
 }
-const computerSelection = getComputerChoice();
 
 // PLAYER SELECTION
 function getPlayerChoice() {
     return prompt("Rock, paper, or scissors?");
 }
-const playerSelection = getPlayerChoice();
 
 // ONE ROUND
 function playRound(playerSelection, computerSelection) {
-    console.log(`Computer chose ${computerSelection}.`); 
-// DRAWS
-    if (computerSelection === "Rock" && playerSelection.toLowerCase() === "rock"){
-        return "Draw.";
-    } else if (computerSelection === "Paper" && playerSelection.toLowerCase() === "paper"){
-        return "Draw.";
-    } else if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "scissors"){
-        return "Draw."; 
-// PLAYER WINS    
-    } else if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "rock"){
-        return "You win!";
-    } else if (computerSelection === "Rock" && playerSelection.toLowerCase() === "paper"){
-        return "You win!";
-    } else if (computerSelection === "Paper" && playerSelection.toLowerCase() === "scissors"){
-        return "You win!";  
-// COMPUTER WINS    
-    } else if (computerSelection === "Rock" && playerSelection.toLowerCase() === "scissors"){
-        return "We have a LOSER!";
-    } else if (computerSelection === "Paper" && playerSelection.toLowerCase() === "rock"){
-        return "We have a LOSER!";
-    } else if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "paper"){
-        return "We have a LOSER!";
+    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+        return "Draw";
+    } else if ((computerSelection === "Scissors" && playerSelection.toLowerCase() === "rock") ||
+               (computerSelection === "Rock" && playerSelection.toLowerCase() === "paper") ||
+               (computerSelection === "Paper" && playerSelection.toLowerCase() === "scissors")) {
+        return "Win";
+    } else {
+        return "Loss";
     }
 }
-let roundResult = playRound(playerSelection, computerSelection)
 
 // GAME
 let playerScore = 0
 let compScore = 0
 
-function playGame(roundResult, playerScore, compScore){
-    if (roundResult === "You win!") {
+function playGame(){
+    let playerSelection = getPlayerChoice().toLowerCase();
+    let computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection, computerSelection)
+    console.log(`Computer chose ${computerSelection}. You chose ${playerSelection}.`)
+    if (roundResult == "Win") {
         ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "We have a LOSER!") {
+    } else if (roundResult == "Loss") {
         ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "Draw.") {
+    } else if (roundResult == "Draw") {
         console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
-    };
-    console.log(getPlayerChoice());
-    console.log(playRound(playerSelection, computerSelection));
-    if (roundResult === "You win!") {
-        ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "We have a LOSER!") {
-        ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "Draw.") {
-        console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
-    };
-    console.log(getPlayerChoice());
-    console.log(playRound(playerSelection, computerSelection));
-    if (roundResult === "You win!") {
-        ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "We have a LOSER!") {
-        ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "Draw.") {
-        console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
-    };
-    console.log(getPlayerChoice())
-    console.log(playRound(playerSelection, computerSelection))
-    if (roundResult === "You win!") {
-        ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "We have a LOSER!") {
-        ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "Draw.") {
-        console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
-    };
-    console.log(getPlayerChoice());
-    console.log(playRound(playerSelection, computerSelection));
-    if (roundResult === "You win!") {
-        ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "We have a LOSER!") {
-        ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult === "Draw.") {
-        console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
-    };
-};
+    }
+}
 
-console.log(playGame(roundResult, playerScore, compScore));
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+
+// RESULTS
+if (playerScore > compScore) {
+    console.log('You win! Congratulations!!! Refresh to play again.');
+} else if (playerScore < compScore) {
+    console.log('You lose! Sucks to be you!!! Refresh to try again.');
+} else {
+    console.log('The game was a draw. Refresh to try again.');
+}
+
+
