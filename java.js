09971564a6746c6747e8c18ac055121f94fac76d
BradQuenneville <quennevillebradley@gmail.com>
2024-04-23 23:41:56 -0700

    Complete whole project.
    Got frustrated and didn't commit often.

--- java.js
+++ java.js
@@ -1,59 +1,81 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+const results = document.querySelector("#results");
+
 // COMPUTER SELECTION
 const choices = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
-    return computerRPS = choices[Math.floor(Math.random() * choices.length)];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-
-// PLAYER SELECTION
-function getPlayerChoice() {
-    return prompt("Rock, paper, or scissors?");
-}
-
-// ONE ROUND
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
-        return "Draw";
-    } else if ((computerSelection === "Scissors" && playerSelection.toLowerCase() === "rock") ||
-               (computerSelection === "Rock" && playerSelection.toLowerCase() === "paper") ||
-               (computerSelection === "Paper" && playerSelection.toLowerCase() === "scissors")) {
-        return "Win";
-    } else {
-        return "Loss";
-    }
-}
+    
 
 // GAME
 let playerScore = 0
 let compScore = 0
 
-function playGame(){
-    let playerSelection = getPlayerChoice().toLowerCase();
-    let computerSelection = getComputerChoice();
-    let roundResult = playRound(playerSelection, computerSelection)
-    console.log(`Computer chose ${computerSelection}. You chose ${playerSelection}.`)
-    if (roundResult == "Win") {
-        ++playerScore, console.log(`Player WINS! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult == "Loss") {
-        ++compScore, console.log(`Player LOSES! Player: ${playerScore}, Computer: ${compScore}`)
-    } else if (roundResult == "Draw") {
-        console.log(`DRAW! Player: ${playerScore}, Computer: ${compScore}`)
+function playGame(playerSelection, computerSelection){
+    if (playerScore < 5 && compScore < 5) {if ((computerSelection === "Scissors" && playerSelection === "Rock") ||
+    (computerSelection === "Rock" && playerSelection === "Paper") ||
+    (computerSelection === "Paper" && playerSelection === "Scissors")) {
+        ++playerScore; 
+        const p = document.createElement('p')
+        p.textContent = `Player WINS! You chose ${playerSelection}. Computer chose ${computerSelection}.
+            Player: ${playerScore}, Computer: ${compScore}`;
+        results.appendChild(p);
+    }  else if (computerSelection === playerSelection) {
+        const p = document.createElement('p')
+        p.textContent = `DRAW! You chose ${playerSelection}. Computer chose ${computerSelection}.
+            Player: ${playerScore}, Computer: ${compScore}`;
+        results.appendChild(p);
+    }  else {
+        ++compScore; 
+        const p = document.createElement('p')
+        p.textContent = `Player LOSES! You chose ${playerSelection}. Computer chose ${computerSelection}.
+            Player: ${playerScore}, Computer: ${compScore}`;
+        results.appendChild(p);
     }
 }
-
-console.log(playGame());
-console.log(playGame());
-console.log(playGame());
-console.log(playGame());
-console.log(playGame());
+}
 
 // RESULTS
-if (playerScore > compScore) {
-    console.log('You win! Congratulations!!! Refresh to play again.');
-} else if (playerScore < compScore) {
-    console.log('You lose! Sucks to be you!!! Refresh to try again.');
-} else {
-    console.log('The game was a draw. Refresh to try again.');
-}
+const checkWinner = (playerScore, compScore) => {
+if (playerScore === 5) {
+    const h2 = document.createElement('h2')
+    h2.classList.add("playerWin")
+    h2.textContent = 'You win! Congratulations!!! Refresh to play again.';
+    h2.style.cssText = "background:green";
+    results.appendChild(h2);
+} else if (compScore === 5) {
+    const h2 = document.createElement('h2')
+    h2.classList.add("playerLoss")
+    h2.textContent = 'You lose! Sucks to be you!!! Refresh to try again.';
+    h2.style.cssText = "background:red";
+    results.appendChild(h2);
+};
+};
+
+// BUTTONS
+
+rockBtn.addEventListener("click", () => {
+    const playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+    playGame(playerSelection, computerSelection);
+    checkWinner(playerScore, compScore);
+})
+paperBtn.addEventListener("click", () => {
+    const playerSelection = "Paper"
+    const computerSelection = getComputerChoice();
+    playGame(playerSelection, computerSelection);
+    checkWinner(playerScore, compScore);
+})
+scissorsBtn.addEventListener("click", () => {
+    const playerSelection = "Scissors"
+    const computerSelection = getComputerChoice();
+    playGame(playerSelection, computerSelection);
+    checkWinner(playerScore, compScore);
+})
 
 
